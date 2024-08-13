b4b4c7f6192143788ad0e5aeae3af525fd4f5ff0
KarolBedoya <karsbedoya1@misena.edu.co>
2024-08-13 14:41:04 -0500

    index productos

--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -14,7 +14,7 @@
                 <img style="height: 300px; width:200px; margin:20px" src="{{ Storage::url($producvisto->imagen) }}" class="card-img-top mx-auto d-block" alt="...">
                 <div class="card-body">
                   <h5 class="card-title">{{ $producvisto->nombre }}</h5>
-                  <p class="card-text">{{ $producvisto->precio }}</p>
+                  <p class="card-text">$ {{ $producvisto->precio }}</p>
                   <a href="/productos/{{$producvisto->id}}" class="btn">Descripción</a>
                 </div>
             </div>
